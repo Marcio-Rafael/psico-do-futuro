bce3f8b1234f1ab996d333a438d819b7863c7f9f
Marcio <marcio.rafaelssousa2009@hotmail.com>
2024-07-14 13:57:16 -0300

    Reajuste nos cards

--- src/app/components/widget/widget.js
+++ src/app/components/widget/widget.js
@@ -1,37 +1,37 @@
 
 import './Widget.css';
-
+import Image from 'next/image';
 function Widget(props) {
 
   return (
     <div className="widget-container justify-center w-full">
       <div className="bg-white rounded-xl overflow-hidden shadow-lg p-4 ">
         <div className=" py-2">
-          <div className="flex justify-between space-x-2">
-            <img className="h-16 rounded-full" src={props.perfil} alt="foto de perfil do psicologo" />
-            <div className="">
-              <div className="font-extrabold text-1xl text-sky-950 text-left whitespace-nowrap">Angela Cruz de Souza Melquiades</div>
-              <p className="text-gray-500 text-left mt-1 ">Especializacao em psicopatologia CRP:06/170123</p>
+          <div className="flex justify-between space-x-2" alt="foto de perfil do psicologo">
+            <Image src={props.perfil} className="w-16 h-16 rounded-full" alt='Logotipo' />
+            <div>
+              <div className="font-extrabold text-sky-950 w-72 h-5 text-left whitespace-nowrap">Angela Cruz de Souza Melquiades</div>
+              <p className="text-gray-500 w-72 h-8 text-left mt-1 ">Especializacao em psicopatologia CRP:06/170123</p>
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-between">
-          <div className="text-center">
-            <p className="text-gray-600 text-base text-left">Primeira Sessao</p>
-            <span className="inline-block bg-gray-100 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 w-36">R$190,00</span>
+        <div className="flex items-center gap-5 mb-5">
+          <div className="text-center flex-[1_1_50%]">
+            <p className="text-gray-600 text-start text-sm">Primeira Sessao</p>
+            <span className="inline-block bg-gray-100 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 w-full">R$190,00</span>
           </div>
-          <div className="text-center">
-            <p className="text-gray-600 text-base text-left">Plano Mensal</p>
-            <span className="inline-block bg-green-100 rounded-md px-3 py-1 text-sm font-semibold text-green-950 mr-2 mb-2 w-36">R$800,00</span>
+          <div className="text-center flex-[1_1_50%]">
+            <p className="text-gray-600 text-start text-sm ">Plano mensal</p>
+            <span className="inline-block bg-green-100 rounded-md px-3 py-1 text-sm font-semibold text-gray-950 w-full">R$800,00</span>
           </div>
         </div>
         <div>
-          <p className="text-gray-500 text-left" >
+          <p className="text-gray-500 text-left text-sm" >
             Olá, sou a Ângela, psicóloga com foco na abordagem comportamental. Meu objetivo é auxiliar você a se reconectar consigo mesmo, com zelo e...
           </p>
         </div>
         <div>
-          <button className='widget-button' >Ver perfil completo</button>
+          <button className='widget-button text-sm' >Ver perfil completo</button>
         </div>
       </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Widget from './components/widget/widget';
 import './globals.css';
-import Logo from "./assets/images/logotipo.psico.png";
+import Image from 'next/image';
+import logotipo from './assets/images/logotipo.png';
 import Psicologo from "./assets/images/perfil-psico.jpg";
 
 
@@ -9,15 +10,15 @@ export default function Home() {
   return (
     <div className='App px-2 md:px-24 pb-20 bg-red-50 min-h-screen flex flex-col space-y-5'>
       <div className='mt-20'>
-        <img className='h-10' src={Logo} alt='Logotipo' />
+        <Image className='h-6 w-32' src={logotipo} alt='Logotipo' />
       </div>
       <div>
         <div className='text-left mb-24'>
-          <h1 className='text-red-400 text-2xl md:text-6xl font-bold mb-6'>Encontre a profissional<br /> certa e transforme sua vida.</h1>
-          <p className='paragrafo-home text-2xl text-cyan-950'>Encontramos alguns psis que se encaixam em seu perfil,veja mais <br />detalhes de cada profissional e agende sua primeira consulta.</p>
+          <h1 className='text-red-400 md:text-6xl font-bold mb-6'>Encontre a profissional<br /> certa e transforme sua vida.</h1>
+          <p className='paragrafo-home text-xl text-cyan-950'>Encontramos alguns psis que se encaixam em seu perfil,veja mais <br />detalhes de cada profissional e agende sua primeira consulta.</p>
         </div>
         <div className='flex flex-wrap gap-7 justify-center lg:justify-start'>
-          <Widget perfil={Psicologo}>
+          <Widget perfil={Psicologo} >
           </Widget>
           <Widget perfil={Psicologo}>
           </Widget>
